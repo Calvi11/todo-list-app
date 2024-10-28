791df60c8623f8124a65ed45ebb90165ebaaa410
Calvi11 <leecalv02@gmail.com>
2024-10-28 20:52:46 +0800

    Fixed issues based on Lab 7 feedback

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
  
 // define the port
-const port = 4000;
+const port = 3000;
  
 app.get("/", (req, res) => {
   res.send("Hello Todo App!!!");
